66a5f63ebff159c234380df22750ac1171b2ff9c
Sarah Kerr <sarah.kerr93@gmail.com>
2020-06-13 17:28:38 -0400

    added an HTML file, CSS file, and JavaScript file which reference each other as needed. Added question array to JS file

---
+++ script.js
@@ -0,0 +1,42 @@
+var myQuestions = [
+    {
+      question: "Which does JavaScript work with to create interactive webpages online?",
+      answers: {
+        a: "HTML and CSS",
+        b: "CSS and JQuery",
+        c: "HTML only",
+        d: "HTML and your local computer files"
+      },
+      correctAnswer: "a"
+    },
+    {
+      question: "Conditions of an if statement are surrounded on either side by: ",
+      answers: {
+        a: "Curly braces { }",
+        b: "Brackets [ ]",
+        c: "Parentheses ( )",
+        d: 'Quotation marks " "'
+      },
+      correctAnswer: "c"
+    },
+    {
+      question: "Any information stored on the Local Directory lives where?",
+      answers: {
+        a: "It depends on where the central database is geographically located",
+        b: "The browser memory",
+        c: "The hardrive of the user",
+        d: "It is not stored anywhere"
+      },
+      correctAnswer: "b"
+    },
+    {
+        question: "Learning JavaScript is",
+        answers: {
+          a: "Fun",
+          b: "Occassionally confusing",
+          c: "Worth it!",
+          d: "All of the above"
+        },
+        correctAnswer: "d"
+      }
+  ];
