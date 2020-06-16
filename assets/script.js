3690041e8d034b49d0637384f63aa097e469637c
Sarah Kerr <sarah.kerr93@gmail.com>
2020-06-16 18:41:39 -0400

    added some div elements to organize my page and some minor css tweaks

--- assets/script.js
+++ assets/script.js
@@ -1,42 +1,57 @@
+var totalSeconds = 60;
+
 var myQuestions = [
     {
       question: "Which does JavaScript work with to create interactive webpages online?",
-      answers: {
-        a: "HTML and CSS",
-        b: "CSS and JQuery",
-        c: "HTML only",
-        d: "HTML and your local computer files"
-      },
-      correctAnswer: "a"
+      answers: [
+        "HTML and CSS",
+        "CSS and JQuery",
+        "HTML only",
+        "HTML and your local computer files"
+      ],
+      correctAnswer: "HTML and CSS"
     },
     {
       question: "Conditions of an if statement are surrounded on either side by: ",
-      answers: {
-        a: "Curly braces { }",
-        b: "Brackets [ ]",
-        c: "Parentheses ( )",
-        d: 'Quotation marks " "'
-      },
-      correctAnswer: "c"
+      answers: [
+        "Curly braces { }",
+        "Brackets [ ]",
+        "Parentheses ( )",
+        'Quotation marks " "'
+      ],
+      correctAnswer: "Parentheses ( )"
     },
     {
       question: "Any information stored on the Local Directory lives where?",
-      answers: {
-        a: "It depends on where the central database is geographically located",
-        b: "The browser memory",
-        c: "The hardrive of the user",
-        d: "It is not stored anywhere"
-      },
-      correctAnswer: "b"
+      answers: [
+        "It depends on where the central database is geographically located",
+        "The browser memory",
+        "The hardrive of the user",
+        "It is not stored anywhere"
+      ],
+      correctAnswer: "The browser memory"
     },
     {
         question: "Learning JavaScript is",
-        answers: {
-          a: "Fun",
-          b: "Occassionally confusing",
-          c: "Worth it!",
-          d: "All of the above"
-        },
-        correctAnswer: "d"
+        answers: [
+          "Fun",
+          "Occassionally confusing",
+          "Worth it!",
+          "All of the above"
+        ],
+        correctAnswer: "All of the above"
       }
   ];
+
+  // function startTimer() {
+  
+  //   We only want the timer to run if totalSeconds is > 0
+  //   if (totalSeconds > 0) {
+  //     totalSeconds--; 
+  //     So renderTime() is called here once every second.
+  //     renderTime();
+  //       }, 1000);
+  //   } else {
+  //     alert("Minutes of work/rest must be greater than 0.")
+  //   }
+  // }
